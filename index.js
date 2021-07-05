49595b7d2a5afc18c32b93db95d2b49d22fa71d6
jai 3 <47060357+jdadonut@users.noreply.github.com>
2021-07-04 23:04:18 -0400

    fixed thing

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
 const { Plugin } = require('powercord/entities');
 const { findInReactTree } = require("powercord/util");
-const { getModule, React, messages: {deleteMessage} } = require("powercord/webpack");
+const { getModule, React, messages: { deleteMessage } } = require("powercord/webpack");
 const { inject, uninject } = require("powercord/injector");
 // Discord React Bits
 let Message;
 // constant keycodes
-const KEYCODES = {Backspace:8};
+const KEYCODES = { Backspace: 8 };
 module.exports = class QuickDelete extends Plugin {
-    
+
     async startPlugin() {
         console.log("started")
         Message = getModule(m => m?.default?.displayName === "Message", false);
         inject("QuickDelete", Message, (_, res) => {
-            let ce = findInReactTree(res.props.childrenButtons, r => r?.props?.hasOwnProperty("canDelete"))
-            console.log("CE=" + ce);
-            res.addEventListener("mousedown", e => {setTimeout(() => {
-            if (this.keybindDown && this.clicking && ce.props.canDelete)
-            {
-                console.log("dasdwetgwe");
-                this.deleteMessage(findInReactTree(res, r => r?.message).message);
-            }}, 100)})
+            React.createElement("div",
+                {
+                    className: ".quick-delete",
+                    onClick: () => {
+                        let ce = findInReactTree(res.props.childrenButtons, r => r?.props?.hasOwnProperty("canDelete"))
+                        console.log("CE=" + ce);
+                        if (this.keybindDown && this.clicking && ce.props.canDelete) {
+                            console.log("dasdwetgwe");
+                            this.deleteMessage(findInReactTree(res, r => r?.message).message);
+                        }
+                    }
+                });
         })
 
-        document.body.addEventListener("keydown", (e) => {if (e.keyCode == KEYCODES.Backspace) {this.keybindDown = true;}})
-        document.body.addEventListener("keyup", (e) => {if (e.keyCode == KEYCODES.Backspace) {this.keybindDown = false;}})
-        document.body.addEventListener("mousedown", (e) => {if (e.button == 0) {this.mouseDown = true;}})
-        document.body.addEventListener("mouseup", (e) => {if (e.button == 0) {this.mouseDown = false;}})
-        
+
+        document.body.addEventListener("keydown", (e) => { if (e.keyCode == KEYCODES.Backspace) { this.keybindDown = true; } })
+        document.body.addEventListener("keyup", (e) => { if (e.keyCode == KEYCODES.Backspace) { this.keybindDown = false; } })
+        document.body.addEventListener("mousedown", (e) => { if (e.button == 0) { this.mouseDown = true; } })
+        document.body.addEventListener("mouseup", (e) => { if (e.button == 0) { this.mouseDown = false; } })
+
     }
-    deleteMessage(mesg){
+    deleteMessage(mesg) {
         deleteMessage(mesg.channel_id, mesg.id)
     }
 
@@ -36,4 +41,16 @@ module.exports = class QuickDelete extends Plugin {
         uninject("QuickDelete")
     }
 };
+{
+    let ce = findInReactTree(res.props.childrenButtons, r => r?.props?.hasOwnProperty("canDelete"))
+    console.log("CE=" + ce);
+    res.addEventListener("mousedown", e => {
+        setTimeout(() => {
+            if (this.keybindDown && this.clicking && ce.props.canDelete) {
+                console.log("dasdwetgwe");
+                this.deleteMessage(findInReactTree(res, r => r?.message).message);
+            }
+        }, 100)
+    })
+}
 // findInReactTree(Message.props.childrenButtons, r => r?.hasOwnProperty("canDelete"))
