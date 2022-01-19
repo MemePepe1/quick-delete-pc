fc256cdae5816d87cc5b00c01e1c050e85c93c8e
llamasking <21324788+llamasking@users.noreply.github.com>
2022-01-19 02:45:26 -0800

    feat: settings page and effective rewrite
    - Added a settings page with the ability to rebind the delete key
    - Clean up messy code
    - Memory optimizations

--- index.js
+++ index.js
@@ -1,44 +1,69 @@
 const { Plugin } = require('powercord/entities');
-const { getModule, React, messages: { deleteMessage }, constants } = require("powercord/webpack");
+const { getModule, getModuleByDisplayName, messages: { deleteMessage }, constants: { Permissions } } = require("powercord/webpack");
 const { inject, uninject } = require("powercord/injector");
 const { can } = getModule(["getChannelPermissions"], false);
-const { getUser, getCurrentUser } = getModule(["getCurrentUser"], false);
-const { Permissions: { MANAGE_MESSAGES } } = constants;
-const canDeleteMessage = (msg, channel) => (msg.author === getCurrentUser() || can(MANAGE_MESSAGES, channel));
-let Message;
-const Activate = 8;
-module.exports = class QuickDelete extends Plugin {
+const { getCurrentUser } = getModule(["getCurrentUser"], false);
+const Settings = require("./components/settings.jsx");
 
+const canDeleteMessage = (msg, channel) => msg.author === getCurrentUser() || can(Permissions.MANAGE_MESSAGES, channel);
+
+module.exports = class QuickDelete extends Plugin {
     async startPlugin() {
-        Message = getModule(m => m?.default?.displayName === "Message", false);
+        powercord.api.settings.registerSettings(this.entityID, {
+            category: this.entityID,
+            label: 'Message Quick-Delete',
+            render: Settings,
+        });
+
+        const Message = getModule(m => m.default?.displayName === "Message", false);
         inject("quick-delete", Message, 'default', (args, res) => {
             // Reassign onClick function (botch)
-            res.props.children.props.oldOnCLick = res.props.children.props.onClick;
-            res.props.children.props.onClick = ((e) => {
-                const { childrenAccessories: { props: { message, channel } } } = args[0];
-                ((e, _this, res) => {
-                    // Call old onClick function to where it doesn't disturb the rest of the code
-                    if (typeof res.props.children.props.oldOnCLick === 'function')
-                        (async () => { res.props.children.props.oldOnCLick(e) })()
-                    // Delete message on keybind down && left click && canDeleteMessage.
-                    if (_this.keybindDown && e.button == 0 && canDeleteMessage(message, channel)) {
-                        _this.deleteMessage(message);
-                    }
-                })(e, this, res)
-            })
+            res.props.children.props.oldOnClick = res.props.children.props.onClick;
+            res.props.children.props.onClick = ((e) => this.handleMessageClick(e, args, res))
             return res;
         })
-        document.body.addEventListener("keydown", (e) => { if (e.keyCode == Activate) { this.keybindDown = true; } })
-        document.body.addEventListener("keyup", (e) => { if (e.keyCode == Activate) { this.keybindDown = false; } })
-        document.body.addEventListener("mousedown", (e) => { if (e.button == 0) { this.mouseDown = true; } })
-        document.body.addEventListener("mouseup", (e) => { if (e.button == 0) { this.mouseDown = false; } })
 
-    }
-    deleteMessage(mesg) {
-        deleteMessage(mesg.channel_id, mesg.id)
+        // Weird quirk
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+        document.body.addEventListener("keydown", this.handleKeyPress);
+        document.body.addEventListener("keyup", this.handleKeyPress);
     }
 
     pluginWillUnload() {
+        powercord.api.settings.unregisterSettings(this.entityID);
+
         uninject("quick-delete")
+
+        document.body.removeEventListener('keydown', this.handleKeyPress);
+        document.body.removeEventListener('keyup', this.handleKeyPress);
+    }
+
+    handleKeyPress(e) {
+        this.keyBindDown = e.code === this.settings.get('qdKey', "Backspace");
+    }
+
+    handleMessageClick(e, args, res) {
+        try {
+            const { childrenAccessories: { props: { message, channel } } } = args[0];
+
+            // Call old onClick function to where it doesn't disturb the rest of the code
+            if (typeof res.props.children.props.oldOnClick === 'function')
+                try {
+                    res.props.children.props.oldOnClick(e)
+                } catch (err) {
+                    this.error("An error occurred in the backed up onClick function. It is unlikely this plugin is the cause. :: " + err)
+                }
+
+            // Delete message on keybind down && left click && canDeleteMessage.
+            if (this.keyBindDown && e.button === 0 && canDeleteMessage(message, channel)) {
+                this.handleMessageDelete(message);
+            }
+        } catch (err) {
+            this.error(err);
+        }
+    }
+
+    handleMessageDelete(msg) {
+        deleteMessage(msg.channel_id, msg.id, false);
     }
 };
